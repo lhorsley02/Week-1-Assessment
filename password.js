b8faf779baad4b609231ca6b0d688a7a8e1db807
Laura Horsley <horsleyshorses02@gmail.com>
2022-02-11 11:23:36 -0800

    Final Commit

---
+++ password.js
@@ -0,0 +1,38 @@
+const readline = require('readline');
+
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+console.log("Welcome to Password Validator Tool!")
+
+reader. question("Please enter the password you wish to validate: ", function(input){
+    tokens = input.split(' ');
+
+    
+    userPass = tokens[0];
+	passLength = userPass.length;
+	passCase = " ";
+
+	console.log('userPass', userPass);
+	console.log('passLength', userPass.length);
+
+
+	if (userPass.length > 10){
+        console.log("password is too long");
+	}
+	if (userPass.length < 10 ){
+        console.log("password is too short")
+	}
+    else{ 
+    	console.log("password is correct length");
+    } 
+	
+    
+	
+
+
+	reader.close()
+
+});
